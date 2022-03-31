eda7cd56d573822acce4335c3b8101bc2546a182
BangGyum <83632850+BangGyum@users.noreply.github.com>
2022-03-31 16:36:12 +0900

    DB insert 성공

--- app/src/main/java/com/banggyum/test/MainActivity.java
+++ app/src/main/java/com/banggyum/test/MainActivity.java
@@ -1,14 +1,36 @@
 package com.banggyum.test;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText title_input, author_input, pages_input;
+    Button add_button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        pages_input = findViewById(R.id.pages_input);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MyDatabaseHelper myDb = new MyDatabaseHelper(MainActivity.this);
+                myDb.addBook(title_input.getText().toString().trim(), author_input.getText().toString().trim(), Integer.parseInt(pages_input.getText().toString().trim()));
+            }
+        });
     }
 }
