cbe6645785b760e7474c80d5cd9c0f0913376b4b
GOMGOMHE <102653475+GOMGOMHE@users.noreply.github.com>
2022-05-02 01:02:38 +0900

    조성윤이 바꿈

--- app/src/main/java/com/banggyum/test/MainActivity.java
+++ app/src/main/java/com/banggyum/test/MainActivity.java
@@ -2,64 +2,57 @@ package com.banggyum.test;
 
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity
-{
-    EditText title_input, author_input, pages_input;
-    Button add_button;
+public class MainActivity extends AppCompatActivity {
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-
-    ActionBarDrawerToggle drawerToggle;
-
+    BottomNavigationView bottomNavigationView; //네비게이션뷰
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 사이드 메뉴바 입니다.
-        // 액션바를 툴바로 대체하기
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        bottomNavigationView = findViewById(R.id.bottomNav);
 
-        drawerLayout = findViewById(R.id.layout_drawer);
-        navigationView = findViewById(R.id.nav);
-        // 네비게이션뷰의 메뉴아이콘의 색조 제거
-        navigationView.setItemIconTintList(null);
+        //네비바 처음화면
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new Fragment_Schedule()).commit();
+        //네비바 안의 아이템 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    //네비바 버튼을 클릭시 id값을 가져와 FrameLayout에 fragment띄우기
+                    case R.id.schedule_fragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment_Schedule()).commit();
+                        break;
+                    case R.id.calender_fragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment_Calendar()).commit();
+                        break;
+                    case R.id.maps_fragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment_Map()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
 
-        // 드로우어 조절용 토글버튼 객체 생성
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.app_name,R.string.app_name); //마지막 두 매개변수는 그냥 써주자
-        drawerToggle.syncState(); //삼선 메뉴 만들기
-        drawerLayout.addDrawerListener(drawerToggle); // 뱅그르 돌게 만들기
-
-        // 액션바에 제목이 자동 표시 되지 않도록
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-//        title_input = findViewById(R.id.title_input);
-//        author_input = findViewById(R.id.author_input);
-//        pages_input = findViewById(R.id.pages_input);
-//        add_button = findViewById(R.id.add_button);
-//        add_button.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                MyDatabaseHelper myDb = new MyDatabaseHelper(MainActivity.this);
-//                myDb.addBook(title_input.getText().toString().trim(), author_input.getText().toString().trim(), Integer.parseInt(pages_input.getText().toString().trim()));
-//            }
-//        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+        return true;
     }
 }
+
